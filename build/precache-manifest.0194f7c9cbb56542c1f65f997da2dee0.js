4b85c27a55df309ba1ea217e459375055a7e2a89
chini5ko <chinisko@outlook.com>
2020-08-04 16:52:35 -0400

    new built

---
+++ build/precache-manifest.0194f7c9cbb56542c1f65f997da2dee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f37ab555e971ee097e7b474afb5cb362",
+    "url": "/index.html"
+  },
+  {
+    "revision": "beef066ec42cefee332b",
+    "url": "/static/css/main.1a179a4f.chunk.css"
+  },
+  {
+    "revision": "c164db0d716c10a5f408",
+    "url": "/static/js/2.f7f5c33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7f5c33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beef066ec42cefee332b",
+    "url": "/static/js/main.d00d25cf.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
