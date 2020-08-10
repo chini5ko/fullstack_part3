5717408e6fd52bc7fcf4c248f20c9b91869b9b25
chini5ko <chinisko@outlook.com>
2020-08-09 22:58:07 -0400

    fixed lint syntax for person.js

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.set('useFindAndModify', false)
-var uniqueValidator = require('mongoose-unique-validator');
+var uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
@@ -8,7 +8,7 @@ console.log('connecting to', url)
 // url  mongodb+srv://fullstack:<password>@cluster0.eusd4.mongodb.net/<dbname>?retryWrites=true&w=majority
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -17,55 +17,16 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const personSchema = new mongoose.Schema({
   name: {
-    type:String, 
+    type:String,
     minlength: 3,
     required: true
   },
   number: {
-    type:String, 
+    type:String,
     minlength: 8,
     required: true
-    
   }
 })
 
-personSchema.plugin(uniqueValidator);
-
-
-
+personSchema.plugin(uniqueValidator)
 module.exports = mongoose.model('Person', personSchema)
-
-// const Person = mongoose.model('Person', personSchema)
-
-
-// if (process.argv.length == 3 ){
-//     Person.find({}).then(result => {
-//       result.forEach(person => {
-//         console.log(person)
-//       }).
-//       mongoose.connection.close()
-//     })
-//   }
-  
-//   if (process.argv.length > 3 ){
-//     const person = new Person({
-//       name: name,
-//       number: number
-//     })
-    
-//     person.save().then(result => {
-//       console.log(`added ${name} number ${number} to phonebook`)
-//       mongoose.connection.close()
-//     })
-//   }
-
-
-// const password = process.argv[2]
-// const name = process.argv[3]
-// const number = process.argv[4]
-
-// if (process.argv.length < 3) {
-//   console.log('Please provide the password as an argument: node mongo.js <password>')
-//   process.exit(1)
-// }
-
